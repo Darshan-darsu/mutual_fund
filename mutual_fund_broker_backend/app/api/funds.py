aab74a63a7f9e7d8e52c0a802d7526a196dec31a
Darshan <darshan.km@affine.ai>
2025-04-27 19:25:08 +0530

    reading from api

--- mutual_fund_broker_backend/app/api/funds.py
+++ mutual_fund_broker_backend/app/api/funds.py
@@ -2,9 +2,11 @@
 from fastapi import APIRouter,Depends
 from app.utils.readfile import read_fund_data
 from app.core.verify_token import verify_token
+from app.services.rapidapi import fetch_fund_data
 
 router = APIRouter()
 
 @router.get("/latest")
 def fetch_funds(user_id: str = Depends(verify_token)):
-    return read_fund_data()
+    return fetch_fund_data()
+# read_fund_data()
